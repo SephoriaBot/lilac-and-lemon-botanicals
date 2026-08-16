b5faa6058bbf6d55bb94432b011f171f7c52d9da
SephoriaBot <withallofthisimdone@gmail.com>
2026-08-16 18:14:14 -0400

    Update products.ts

--- lilac-and-lemon-botanicals/api/products.ts
+++ lilac-and-lemon-botanicals/api/products.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { turso } from './turso';
+import { turso } from './turso.js';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== 'GET') {
